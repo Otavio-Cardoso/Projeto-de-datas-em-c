b5128abdcf31c8a0a5e12f3018ebc39f7b5d695a
curso.ads <curso.ads@LAB06-72>
2022-06-15 20:44:15 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto de datas/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_de_datas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mesextenso = null;
+            Console.WriteLine("Digite a data desejada em números separados por barra: ");
+            var datan = Convert.ToDateTime(Console.ReadLine());
+
+            int datames, datadia, dataano;
+            datames = datan.Month;
+            datadia = datan.Day;
+            dataano = datan.Year;
+            
+            switch (datames)
+            {
+                case 1:
+                    mesextenso = "Janeiro";
+                    break;
+                case 2:
+                    mesextenso = "Fevereiro";
+                    break;
+                case 3:
+                    mesextenso = "Março";
+                    break;
+                case 4:
+                    mesextenso = "Abril";
+                    break;
+                case 5:
+                    mesextenso = "Maio";
+                    break;
+                case 6:
+                    mesextenso = "Junho";
+                    break;
+                case 7:
+                    mesextenso = "Julho";
+                    break;
+                case 8:
+                    mesextenso = "Agosto";
+                    break;
+                case 9:
+                    mesextenso = "Setembro";
+                    break;
+                case 10:
+                    mesextenso = "Outubro";
+                    break;
+                case 11:
+                    mesextenso = "Novembro";
+                    break;
+                case 12:
+                    mesextenso = "Dezembro";
+                    break;
+            }
+
+            Console.WriteLine($" a data é {datadia}/{mesextenso}/{dataano}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
